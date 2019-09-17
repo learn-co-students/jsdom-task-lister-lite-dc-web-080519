7b158d13b92f148fce97ee82a03557baad5d97d9
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-09-17 16:10:24 -0400

    adding Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form');
+  form.addEventListener("submit", subFunc)
 });
+
+
+function subFunc(e){
+  e.preventDefault();
+  let textField = document.querySelector("#new-task-description");
+  let task = textField.value;
+  let taskList = document.querySelector("#tasks");
+  let taskEntry = document.createElement("li");
+  taskEntry.innerText = task;
+  taskList.appendChild(taskEntry);
+  
+}
